140290447361dd2edba76f56489e1a60acddb7c5
ben <bwhitlock@cyberbility.com>
2016-10-31 01:56:04 -0400

    changed routes, home & user included

--- server.js
+++ server.js
@@ -1,26 +1,55 @@
+'use strict';
+
 var express = require('express');
 var stormpath = require('express-stormpath');
 
+var routes = require('./lib/routes');
+
+
+/**
+ * Create the Express application.
+ */
 var app = express();
 
-app.set('views', './views');
+
+/**
+ * Application settings.
+ */
+app.set('views', './lib/views');
 app.set('view engine', 'jade');
 
+/*
+* Serve Static files: CSS, JS, Images
+*/
+app.use(express.static(__dirname + '/public'));
+
+
+/**
+ * Stormpath initialization.
+ */
 app.use(stormpath.init(app, {
   expand: {
     customData: true
   },
-  website: true
+  website: true,
+  web: {
+    login: {
+      enabled: true,
+      nextUri: "/user"
+    }
+  }
 }));
 
-app.get('/', stormpath.getUser, function(req, res) {
-  res.render('home', {
-    title: 'Welcome'
-  });
-});
 
-app.use('/profile',stormpath.loginRequired,require('./profile')());
+/**
+ * Route initialization.
+ */
+app.use('/', routes);
 
+
+/**
+ * Start the web server.
+ */
 app.on('stormpath.ready',function(){
   	console.log('Stormpath Ready');
 	app.listen(process.env.PORT || 3000);
